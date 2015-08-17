5aba782039d271b163b5e93cb585383860396b1f
Matthew Boyette <Dyndrilliac@gmail.com>
2015-08-17 18:24:47 -0400

    Updated to reflect API changes.

--- Scramble.java
+++ Scramble.java
@@ -2,7 +2,7 @@
  * Title: Scramble
  * Author: Matthew Boyette
  * Date: 3/8/2012
- *
+ * 
  * This is a simple yet challenging word puzzle game where the user has three chances to guess a randomly chosen
  * word that has had its letters rearranged in a random order. The user's total score is a number between zero and
  * five, inclusively. It is determined by dividing the user's total number of points by the total number of puzzles
@@ -41,24 +41,24 @@ public class Scramble
 	{
 		ArrayList<String> words = new ArrayList<String>();
 		String filePath = null;
-
+		
 		do
 		{
 			filePath = Support.getFilePath(parent, true, isDebugging);
 		}
 		while ((filePath == null) || filePath.isEmpty());
-
+		
 		Scanner inputStream = null; // Stream object for file input.
-
+		
 		try
 		{
 			// Initialize file stream. If the given path is invalid, an exception is thrown.
 			inputStream = new Scanner(new File(filePath));
-
+			
 			while (inputStream.hasNextLine())
 			{
 				String line = inputStream.nextLine().toLowerCase().trim();
-
+				
 				if (!(line.isEmpty()))
 				{
 					words.add(line);
@@ -79,39 +79,39 @@ public class Scramble
 				inputStream = null;
 			}
 		}
-
+		
 		return words.toArray();
 	}
-
+	
 	public final static void main(final String[] args)
 	{
 		new Scramble(args);
 	}
-
+	
 	protected final static String shuffleWord(final String word)
 	{
 		// Create a list of characters.
 		java.util.List<Character> characters = new ArrayList<Character>();
-
+		
 		// For each character in strWord, add that character to the list.
 		for (char c: word.toCharArray())
 		{
 			characters.add(c);
 		}
-
+		
 		// StringBuilder objects are the most efficient way to perform variable-length string concatenation.
 		StringBuilder output = new StringBuilder(word.length());
-
+		
 		// Pick a letter randomly from the list, remove it from the list, and then append it to the output string.
 		while (characters.size() != 0)
 		{
 			int randPicker = Mathematics.getRandomInteger(0, characters.size(), false);
 			output.append(characters.remove(randPicker));
 		}
-
+		
 		return output.toString();
 	}
-
+	
 	private int					currentGuesses	= 0;
 	private int					currentScore	= 0;
 	private int					currentWord		= 0;
@@ -122,23 +122,23 @@ public class Scramble
 	private int					totalWords		= 0;
 	private ApplicationWindow	window			= null;
 	private Object[]			wordsArray		= null;
-
+	
 	public Scramble(final String[] args)
 	{
 		this.setDebugging(Support.promptDebugMode(this.getWindow()));
 		this.setWordsArray(Scramble.getWordBank(this.getWindow(), this.isDebugging()));
-
+		
 		// Define a self-contained ActionListener event handler.
 		EventHandler<Scramble> myActionPerformed = new EventHandler<Scramble>(this)
-			{
+		{
 			private final static long	serialVersionUID	= 1L;
-
+			
 			@Override
 			public final void run(final AWTEvent event)
 			{
 				ActionEvent actionEvent = (ActionEvent)event;
 				Scramble parent = this.getParent();
-
+				
 				/*
 				 * JDK 7 allows string objects as the expression in a switch statement.
 				 * This generally produces more efficient byte code compared to a chain of if statements.
@@ -147,17 +147,17 @@ public class Scramble
 				switch (actionEvent.getActionCommand())
 				{
 					case "Guess":
-
+						
 						parent.handleInput();
 						break;
-
+					
 					case "Give Up":
-
+						
 						parent.revealAnswer(parent.getWindow());
 						break;
-
+					
 					default:
-
+						
 						if (actionEvent.getSource() instanceof JTextField)
 						{
 							parent.handleInput();
@@ -165,57 +165,57 @@ public class Scramble
 						break;
 				}
 			}
-			};
-
-			// Define a self-contained interface construction event handler.
-			EventHandler<Scramble> myDrawGUI = new EventHandler<Scramble>(this)
-				{
-				private final static long	serialVersionUID	= 1L;
-
-				@Override
-				public final void run(final ApplicationWindow window)
-				{
-					Scramble parent = this.getParent();
-					Container contentPane = window.getContentPane();
-
-					parent.pickWord();
-
-					JPanel outputPanel = new JPanel();
-					JLabel scrambledLabel = new JLabel("Scrambled Word:   " + parent.getScrambledWord());
-					JPanel textPanel = new JPanel();
-					JTextField inputText = new JTextField(30);
-					JPanel controlPanel = new JPanel();
-					JButton guessButton = new JButton("Guess");
-					JButton giveUpButton = new JButton("Give Up");
-
-					contentPane.setLayout(new BorderLayout());
-					outputPanel.setLayout(new FlowLayout());
-					outputPanel.add(scrambledLabel);
-					contentPane.add(outputPanel, BorderLayout.NORTH);
-					textPanel.setLayout(new FlowLayout());
-					inputText.addActionListener(window);
-					inputText.setFont(Support.DEFAULT_TEXT_FONT);
-					scrambledLabel.setFont(Support.DEFAULT_TEXT_FONT);
-					textPanel.add(inputText);
-					contentPane.add(textPanel, BorderLayout.CENTER);
-					controlPanel.setLayout(new FlowLayout());
-					guessButton.addActionListener(window);
-					guessButton.setFont(Support.DEFAULT_TEXT_FONT);
-					controlPanel.add(guessButton);
-					giveUpButton.addActionListener(window);
-					giveUpButton.setFont(Support.DEFAULT_TEXT_FONT);
-					controlPanel.add(giveUpButton);
-					contentPane.add(controlPanel, BorderLayout.SOUTH);
-					parent.setInput(inputText);
-					parent.setLabel(scrambledLabel);
-				}
-				};
-
-				this.setWindow(new ApplicationWindow(null, this.getTitle(), new Dimension(380, 125), this.isDebugging(),
-					false, myActionPerformed, myDrawGUI));
-				this.getWindow().setIconImageByResourceName("icon.png");
+		};
+		
+		// Define a self-contained interface construction event handler.
+		EventHandler<Scramble> myDrawGUI = new EventHandler<Scramble>(this)
+		{
+			private final static long	serialVersionUID	= 1L;
+			
+			@Override
+			public final void run(final ApplicationWindow window)
+			{
+				Scramble parent = this.getParent();
+				Container contentPane = window.getContentPane();
+				
+				parent.pickWord();
+				
+				JPanel outputPanel = new JPanel();
+				JLabel scrambledLabel = new JLabel("Scrambled Word:   " + parent.getScrambledWord());
+				JPanel textPanel = new JPanel();
+				JTextField inputText = new JTextField(30);
+				JPanel controlPanel = new JPanel();
+				JButton guessButton = new JButton("Guess");
+				JButton giveUpButton = new JButton("Give Up");
+				
+				contentPane.setLayout(new BorderLayout());
+				outputPanel.setLayout(new FlowLayout());
+				outputPanel.add(scrambledLabel);
+				contentPane.add(outputPanel, BorderLayout.NORTH);
+				textPanel.setLayout(new FlowLayout());
+				inputText.addActionListener(window);
+				inputText.setFont(Support.DEFAULT_TEXT_FONT);
+				scrambledLabel.setFont(Support.DEFAULT_TEXT_FONT);
+				textPanel.add(inputText);
+				contentPane.add(textPanel, BorderLayout.CENTER);
+				controlPanel.setLayout(new FlowLayout());
+				guessButton.addActionListener(window);
+				guessButton.setFont(Support.DEFAULT_TEXT_FONT);
+				controlPanel.add(guessButton);
+				giveUpButton.addActionListener(window);
+				giveUpButton.setFont(Support.DEFAULT_TEXT_FONT);
+				controlPanel.add(giveUpButton);
+				contentPane.add(controlPanel, BorderLayout.SOUTH);
+				parent.setInput(inputText);
+				parent.setLabel(scrambledLabel);
+			}
+		};
+		
+		this.setWindow(new ApplicationWindow(null, this.getTitle(), new Dimension(380, 125), this.isDebugging(),
+			false, myActionPerformed, myDrawGUI));
+		this.getWindow().setIconImageByResourceName("icon.png");
 	}
-
+	
 	protected final double calculateScore()
 	{
 		if (this.getTotalWords() == 0) // Cannot divide by zero!
@@ -227,7 +227,7 @@ public class Scramble
 			return ((double)this.getCurrentScore() / (double)this.getTotalWords());
 		}
 	}
-
+	
 	protected final boolean checkGuess(final String guess)
 	{
 		// If 'guess' is the currently scrambled word, the result is true. Otherwise, the result is false.
@@ -240,57 +240,57 @@ public class Scramble
 			return false;
 		}
 	}
-
+	
 	protected final int getCurrentGuesses()
 	{
 		return this.currentGuesses;
 	}
-
+	
 	protected final int getCurrentScore()
 	{
 		return this.currentScore;
 	}
-
+	
 	protected final int getCurrentWord()
 	{
 		return this.currentWord;
 	}
-
+	
 	public final JTextField getInput()
 	{
 		return this.input;
 	}
-
+	
 	public final JLabel getLabel()
 	{
 		return this.label;
 	}
-
+	
 	public final String getScrambledWord()
 	{
 		return this.scrambledWord;
 	}
-
+	
 	protected final String getTitle()
 	{
 		return "Score: " + String.format("%.2f", this.calculateScore()) + "        Puzzles: " + this.getTotalWords();
 	}
-
+	
 	protected final int getTotalWords()
 	{
 		return this.totalWords;
 	}
-
+	
 	public final ApplicationWindow getWindow()
 	{
 		return this.window;
 	}
-
+	
 	protected final Object[] getWordsArray()
 	{
 		return this.wordsArray;
 	}
-
+	
 	public final void handleInput(final Object... arguments)
 	{
 		if (this.getInput() == null)
@@ -304,12 +304,12 @@ public class Scramble
 				Support.displayException(this.window, new Exception("Can't get the input element's text!"), true);
 				return;
 			}
-
+		
 		// Check the user's guess.
 		if (this.checkGuess(this.getInput().getText()))
 		{
 			JOptionPane.showMessageDialog(this.window, "Congratulations, your guess was correct!", "Correct!", JOptionPane.INFORMATION_MESSAGE);
-
+			
 			if (this.getCurrentGuesses() == 3)
 			{
 				this.setCurrentScore(this.getCurrentScore() + 5);
@@ -324,26 +324,26 @@ public class Scramble
 					{
 						this.setCurrentScore(this.getCurrentScore() + 1);
 					}
-
+			
 			this.reinitialize(this.window);
 		}
 		else
 		{
 			JOptionPane.showMessageDialog(this.window, "Sorry, your guess was incorrect!", "Incorrect!", JOptionPane.INFORMATION_MESSAGE);
 			this.setCurrentGuesses(this.getCurrentGuesses() - 1);
-
+			
 			if (this.getCurrentGuesses() <= 0)
 			{
 				this.revealAnswer(this.window);
 			}
 		}
 	}
-
+	
 	public final boolean isDebugging()
 	{
 		return this.isDebugging;
 	}
-
+	
 	public final void pickWord()
 	{
 		// Randomly select a word from the master word list.
@@ -353,7 +353,7 @@ public class Scramble
 		// Set the number of available guesses.
 		this.setCurrentGuesses(3);
 	}
-
+	
 	protected final void reinitialize(final ApplicationWindow window)
 	{
 		// Increment the puzzle counter.
@@ -366,63 +366,63 @@ public class Scramble
 		this.getInput().grabFocus();
 		window.setTitle(this.getTitle());
 	}
-
+	
 	public final void revealAnswer(final ApplicationWindow window)
 	{
 		JOptionPane.showMessageDialog(window,
 			"Puzzle:   " + this.getScrambledWord() + "\n" +
 				"Solution: " + this.getWordsArray()[this.getCurrentWord()] + "\n\n" +
 				"Better luck next time!\n",
-				"Solution to Puzzle",
-				JOptionPane.INFORMATION_MESSAGE);
+			"Solution to Puzzle",
+			JOptionPane.INFORMATION_MESSAGE);
 		this.reinitialize(window);
 	}
-
+	
 	protected final void setCurrentGuesses(final int currentGuesses)
 	{
 		this.currentGuesses = currentGuesses;
 	}
-
+	
 	protected final void setCurrentScore(final int currentScore)
 	{
 		this.currentScore = currentScore;
 	}
-
+	
 	protected final void setCurrentWord(final int currentWord)
 	{
 		this.currentWord = currentWord;
 	}
-
+	
 	protected final void setDebugging(final boolean isDebugging)
 	{
 		this.isDebugging = isDebugging;
 	}
-
+	
 	protected final void setInput(final JTextField input)
 	{
 		this.input = input;
 	}
-
+	
 	protected final void setLabel(final JLabel label)
 	{
 		this.label = label;
 	}
-
+	
 	protected final void setScrambledWord(final String scrambledWord)
 	{
 		this.scrambledWord = scrambledWord;
 	}
-
+	
 	protected final void setTotalWords(final int totalWords)
 	{
 		this.totalWords = totalWords;
 	}
-
+	
 	protected final void setWindow(final ApplicationWindow window)
 	{
 		this.window = window;
 	}
-
+	
 	protected final void setWordsArray(final Object[] wordsArray)
 	{
 		this.wordsArray = wordsArray;
